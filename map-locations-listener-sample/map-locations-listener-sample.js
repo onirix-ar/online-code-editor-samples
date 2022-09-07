0aed25211fa378d1561d3c8726d37a35997520a1
Víctor Fernández <victor.fernandez@onirix.com>
2022-09-07 08:07:29 +0200

    ONIRIX-1169 Map location listener sample

---
+++ map-locations-listener-sample/map-locations-listener-sample.js
@@ -0,0 +1,54 @@
+
+const script = document.createElement('script');
+
+const addListener = (htmlCollection) => {
+    for (let i = 0; i < htmlCollection.length; i++) {
+        const item = htmlCollection.item(i);
+        if (null != item) {
+            item.addEventListener('click', (event) => {
+                for (let j = 0; j < event.target.classList.length; j++) {
+                    const className = event.target.classList.item(j);
+                    if (className.startsWith('ox-map-location--')) {
+                        const sceneOid = className.substring(className.indexOf('--') + 2);
+                        console.log(`onx-sample ->  I'm scene ${sceneOid}`);
+                    }
+                 }
+            });
+        } else {
+            console.log(`onx-sample -> item is null`);
+        }
+    }
+}
+
+const getLocations = () => {
+    const locations = document.getElementsByClassName('ox-map-location');
+    if (locations.length > 0) {
+        addListener(locations);
+        console.log(`onx-sample -> ${(new Date()).toISOString()} Lotations found`);
+        return true;
+    } else {
+        console.log(`onx-sample -> ${(new Date()).toISOString()} Lotations not found`);
+        return false;
+    }
+}
+
+
+script.src = 'https://unpkg.com/@onirix/embed-sdk';
+script.onload = async () => {
+
+    const embedSDK = new OnirixEmbedSDK();
+    embedSDK.connect();
+
+    if (!getLocations()) {
+        const interval = window.setInterval(
+            () => {
+                if (getLocations()) {
+                    clearInterval(interval);
+                }
+            }
+            , 1000);
+    }
+
+
+};
+document.head.appendChild(script);
