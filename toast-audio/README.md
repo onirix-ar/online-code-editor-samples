9fd4db7ed97d562b6da23083237efc34da9cf5c1
Laura Fernández <laura.fernandez@onirix.com>
2023-01-24 08:08:24 +0100

    ENG-27 toast audio sample

---
+++ toast-audio/README.md
@@ -0,0 +1,12 @@
+# Customize sensors
+
+Using this example you can indicate that an experience has audio.
+
+<p style = 'text-align:center;'>
+  <image
+    src="toast-audio.png"
+    alt="Toast Audio"
+    caption="Toast Audio" 
+    style="border-radius: 12px;"
+    >
+</p>
---
+++ toast-audio/toast-audio.js
@@ -0,0 +1,34 @@
+
+/**
+ * Onirix Embed SDK allows you to listen to events and control the scene when embedding experiences in a custom domain or from the online code editor.
+ * For more information visit https://docs.onirix.com/onirix-sdk/embed-sdk
+ */
+import OnirixEmbedSDK from "https://unpkg.com/@onirix/embed-sdk@1.2.3/dist/ox-embed-sdk.esm.js";
+const embedSDK = new OnirixEmbedSDK();
+await embedSDK.connect();
+
+/**
+ * HTML Constants
+*/ 
+const OX_AUDIO = 'ox-audio';
+const CLOSE = 'ox-audio__close';
+
+/**
+ * Indicates the time that the card is available
+ */
+const availableTime = 3000;
+
+
+/**
+* It's execute when the scene is totally load and it start the game
+*/
+embedSDK.subscribe(OnirixEmbedSDK.Events.SCENE_LOAD_START, async (params) => {   
+    document.getElementById(OX_AUDIO).style.display = 'block';
+    setTimeout(() => {
+        document.getElementById(OX_AUDIO).style.display = 'none';
+    }, availableTime);
+
+    document.getElementById(CLOSE).addEventListener('click',() => {
+        document.getElementById(OX_AUDIO).style.display = 'none';
+    })
+});
