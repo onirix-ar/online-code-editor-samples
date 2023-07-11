608e1af82a08b1d2334e21b886e7d001e2ac0cb9
Laura Fernández García <laura.fernandez@onirix.com>
2023-07-11 10:23:52 +0200

    ENG-712 add Amazon Polly sample

--- web-speech-api/README.md
+++ web-speech-api/README.md
@@ -1,7 +1,27 @@
 # Integration with Web Seepech API
 
-Using this example you can integrate Web Speech API with Onirix usign code editor.
+Using this example you can integrate Web Speech API or Amazon Polly with Onirix usign code editor.
+
 Web Speech Api enables you to incorporate voice data into web apps. There are two possibilities, the first is text-to-speech, i.e. the browser will read text, and the second is speech recognition, i.e. the user speaks and the browser processes the words. In this example, the first option is used.
 
-In this example there are two labels called en and es.  Click on each of them to read the text in the corresponding language.
-The second parameter can be removed in the function calls as language =navigator.language would take the language of the browser.
+Amazon Polly do the same as WebSpeech API but provide more uniformity between browsers and operating systems.
+To use Amazon Polly you need an account in it (https://aws.amazon.com/es/polly/) and generate a credential. This credetial is the one you must enter in the code in the "yout credential token" part and set your selected region.
+
+In this examples, the text is going to be read when the scene is loaded and when you click and element.
+A UI with two buttons has been added to be able to stop the locution or change the language at runtime.
+
+If you want to use Web Speech API and change the original language to ES you must initialize like this:
+- const oxSpeech = new OxSpeech(false, {language: "es-ES"});
+
+On the other hand, if you use Amazon Polly and want to change the original language you must initialize like this:
+- const oxSpeech = new OxSpeech(true, {region: "your region", credential: "your credential token", language: "es-ES", voice: "Enrique"});
+
+The UI of this example is:
+<p style = 'text-align:center;'>
+  <image
+    src="web-speech-api.png"
+    alt="Web Speech API"
+    caption="Web Speech API" 
+    style="border-radius: 12px;"
+    >
+</p>
--- web-speech-api/web-speech-api.js
+++ web-speech-api/web-speech-api.js
@@ -1,38 +1,234 @@
-const synth = window.speechSynthesis;
-const spanish_text = "Usted esta escuchando un texto en español.";
-const english_text = "You are listening to a text in English.";
+
+const texts = [
+    {
+        language: "en-GB",
+        click: "You have clicked on the item ",
+        load: "Scene loading is complete"
+    },
+    {
+        language: "es-ES",
+        click: "Has hecho click en el elemento ",
+        load: "Carga de la escena completa"
+    }
+]
 
 /**
- * Generates an object of type SpeechSynthensisUtterance
- * assigning the text to be read and the language.
- * If the language is uninformed, the language of the
- * browser is taken.
- * 
- * @param   text to read
- * @param   language in which to read
+ * This class is in charge of handling the interaction with the UI (only necessary if you have any UI).
  */
-function oxReadString(text, language = navigator.language) {
-    const utterThis = new SpeechSynthesisUtterance(text);
-    utterThis.lang = language
-    synth.speak(utterThis);
+class OxExperienceUi {
+
+    /**
+     * HTML ids
+     */
+    CHANGE = "change";
+    STOP = "stop";
+    
+    /**
+     * Add listeners to the two buttons
+     */
+    initUI() {
+        document.getElementById(this.CHANGE).addEventListener("click", (params) => {
+            if (params.target.innerText == "Change to ES") {
+                document.getElementById(this.CHANGE).innerText = "Cambiar a EN";
+                document.getElementById(this.STOP).innerText = "Parar";
+                this.onChangeLanguage('es-ES');
+                this.onChangeVoice("Enrique");
+            } else {
+                document.getElementById(this.CHANGE).innerText = "Change to ES";
+                document.getElementById(this.STOP).innerText = "Stop";
+                this.onChangeLanguage('en-GB');
+                this.onChangeVoice("Brian");
+            }
+        })
+
+        document.getElementById(this.STOP).addEventListener("click", (params) => {
+            this.onStop();
+        })
+    }
 }
+
+/**
+ * This class is in charge of the communication with the APIs to read the text
+ */
+class OxSpeech {
+
+    /**
+     * Global variables
+     */
+    useAmazon = false;
+    speak = null;
+    synth = null;
+    utterThis = null;
+    language = "en-GB";
+    voice = "Brian";
+
+    /**
+     * Initialize de connection with both APIs depending on the params
+     * 
+     * @param   indicates if the text should be read with Amazon Polly or WebSpeech API
+     * @param   info to establish the connectioin with Amazon Polly, language and voice.
+     * The available voices will depend on the configuration you have in Amazon Polly. Voice list: https://docs.aws.amazon.com/polly/latest/dg/voicelist.html
+     */
+    constructor(useAmazon = false, params = { region: "", credential: "", language: "en-GB", voice: "Brian"}) {
+        this.useAmazon = useAmazon;
+        if (params.language) {
+            this.language = params.language;
+        }
+        if (params.voice) {
+            this.voice = params.voice;
+        }
+
+        if (useAmazon) {
+            if (params.region == "" || params.credential == "") {
+                console.log("Error: region and credential must be informed.")
+            } else {
+                const script = document.createElement("script");
+                script.src = "https://sdk.amazonaws.com/js/aws-sdk-2.1401.0.min.js";
+                script.onload = async () => {
+                    this.speak = (text) => {
+                        // Initialize the Amazon Cognito credentials provider
+                        AWS.config.region = params.region;
+                        AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: `${params.region}:${params.credential}`});
+                        // Create the JSON parameters for getSynthesizeSpeechUrl
+                        const speechParams = {
+                            OutputFormat: "mp3",
+                            SampleRate: "16000",
+                            Text: text,
+                            TextType: "text",
+                            VoiceId: this.voice,
+                            LanguageCode: this.language
+                        };
+                        
+                        // Create the Polly service object and presigner object
+                        const polly = new AWS.Polly({apiVersion: "2016-06-10"});
+                        const signer = new AWS.Polly.Presigner(speechParams, polly);
+
+                        // Create presigned URL of synthesized speech file
+                        const audioPlayback = document.querySelector("#audioPlayback");
+                        const audioSource = document.querySelector("#audioSource");
+                        signer.getSynthesizeSpeechUrl(speechParams, (error, url) => {
+                            if (error) {
+                                console.error(error);
+                            } else {
+                                audioSource.src = url;
+                                audioPlayback.load();
+                                audioPlayback.play();
+                            }
+                        });
+                    }
+
+                };
+                document.head.appendChild(script);
+            }
+        } else {
+            this.synth = window.speechSynthesis;
+            this.utterThis = new SpeechSynthesisUtterance();
+        }
+    }
+
+    /**
+     * Read the text. In case Amazon Polly is used, it calls to speak function inirialized in the constructor
+     * 
+     * @internal
+     * @param   text to read
+     */
+    textToSpeech(text) {
+        if (this.useAmazon) {
+            this.speak(text);
+        } else {
+            this.utterThis.text = text;
+            this.utterThis.lang = this.language
+            this.synth.speak(this.utterThis);
+        }
+    }
+
+    /**
+     * Cancel reading text
+     */
+    stop() {
+        if (this.useAmazon) {
+            const audioPlayback = document.querySelector("#audioPlayback");
+            audioPlayback.pause();
+            audioPlayback.currentTime = 0;
+        } else {
+            this.synth.cancel(this.utterThis)
+        }
+    }
+
+    /**
+     * Read a text when the scene is loaded
+     */
+    onLoad() {
+        const text = texts.find(tex => tex.language == this.language).load;
+        this.textToSpeech(text);
+    }
+
+    /**
+     * Read a text when an element is clicked
+     */
+    onClick(item) {
+        const text = texts.find(tex => tex.language == this.language).click;
+        this.textToSpeech(text + item);
+    }
+
+    /**
+     * Change the language at runtime
+     */
+    setLanguage(language) {
+        this.language = language;
+    }
+
+    /**
+     * Change the voice at runtime
+     */
+    setVoice(voice) {
+        this.voice = voice;
+    }
+
+};
 
 /**
  * Onirix Embed SDK allows you to listen to events and control the scene when embedding experiences in a custom domain or from the online code editor.
  * For more information visit https://docs.onirix.com/onirix-sdk/embed-sdk
  */
 import OnirixEmbedSDK from "https://unpkg.com/@onirix/embed-sdk@1.2.3/dist/ox-embed-sdk.esm.js";
-const embedSDK = new OnirixEmbedSDK(null, "https://stage.onirix.com");
+const embedSDK = new OnirixEmbedSDK();
 await embedSDK.connect();
+const oxExperienceUi = new OxExperienceUi();
 
+/**
+ * Commenting and uncommenting these two lines you can try using WebSpeech API (first line) or Amazon Polly (second line)
+ */
+const oxSpeech = new OxSpeech();
+/*const oxSpeech = new OxSpeech(true, {region: "eu-west-3", credential: "your credential token"});*/
+
+/**
+ * These three functions establish the comunication between the UI and the speech class.
+ * They are only necessary if you want to change the language, voice or stop the locution (like the UI buttons)
+ */
+oxExperienceUi.onChangeLanguage = (language) => {
+    oxSpeech.setLanguage(language);
+}
+
+oxExperienceUi.onChangeVoice = (voice) => {
+    oxSpeech.setVoice(voice);
+}
+
+oxExperienceUi.onStop = () => {
+    oxSpeech.stop();
+}
     
+/**
+ * It's executed when click an AR element
+ */
+embedSDK.subscribe(OnirixEmbedSDK.Events.ELEMENT_CLICK, (params) => {
+    oxSpeech.onClick(params.name);
+})
+
 /**
  * It's executed when the scene is totally loaded
  */
-embedSDK.subscribe(OnirixEmbedSDK.Events.ELEMENT_CLICK, (params) => {
-    if (params.name == "es") {
-        oxReadString(spanish_text, "es-ES");
-    } else {
-        oxReadString(english_text, "en-GB");
-    }
+embedSDK.subscribe(OnirixEmbedSDK.Events.SCENE_LOAD_END, (params) => {
+    oxExperienceUi.initUI();
+    oxSpeech.onLoad();
 })
