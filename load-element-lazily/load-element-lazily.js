03d13a211a2b0969cc47a135ec3532185b875d97
fgarcia <francisco.garcia@onirix.com>
2023-08-16 14:37:18 +0200

    ENG-731 Add lazy loading and dynamic element creation example

---
+++ load-element-lazily/load-element-lazily.js
@@ -0,0 +1,10 @@
+import OnirixEmbedSDK from "https://unpkg.com/@onirix/embed-sdk@latest/dist/ox-embed-sdk.esm.js";
+
+const embedSDK = new OnirixEmbedSDK();
+embedSDK.connect();
+
+embedSDK.subscribe(OnirixEmbedSDK.Events.SCENE_CLICK, async (params) => {
+    console.log('Loading element.')
+    await embedSDK.loadElement('YOUR ELEMENT OID');
+    console.log('element loaded.');
+});
